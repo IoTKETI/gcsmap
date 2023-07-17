77a46b9d0c640e7e432944bea9c735b3de98ee4c
dnjstjr93 <dnjstjr93@gmail.com>
2023-07-17 14:29:21 +0900

    - WebRTC 페이지 변경

--- src/components/GcsAppBar.vue
+++ src/components/GcsAppBar.vue
@@ -890,7 +890,7 @@
                 this.open = !this.open;
                 console.log(this.open);
 
-                window.open("https://13.209.34.14", "_blank");
+                window.open("http://webrtc.iotocean.org", "_blank");
             },
 
             postDroneInfos(callback) {
